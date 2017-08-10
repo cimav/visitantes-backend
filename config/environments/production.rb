754b75a1443aa93799f1cc8b30c07116c2f7142b
Jonathan Hernandez <ion@suavizado.com>
2017-08-10 10:27:46 -0600

    Ajustes en production.rb para que pueda correr en production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+=begin
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -75,4 +78,30 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+=end
+
+config.cache_classes = false
+config.eager_load = false
+config.consider_all_requests_local = true
+if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+        'Cache-Control' => 'public, max-age=172800'
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.perform_caching = false
+config.active_support.deprecation = :log
+config.active_record.migration_error = :page_load
+config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+
 end
