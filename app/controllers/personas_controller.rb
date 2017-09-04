da19e499eba5b5cde9496fa9bcf62664f0d1ab21
JuanCalderon <Pest1lozzi>
2017-09-04 12:01:59 -0600

    Subcontratados ordenados

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -2,14 +2,14 @@ class PersonasController < ApplicationController
 
   def index
 
-    personas = Persona.all
+    personas = Persona.all.order(:nombre)
 
     render json: personas
   end
 
   def proveedores
 
-    personas = Persona.where("tipo = 2 AND sede = :sede", {:sede=>params[:sede]})
+    personas = Persona.where("tipo = 2 AND sede = :sede", {:sede=>params[:sede]}).order(:nombre)
 
     render json: personas
   end
