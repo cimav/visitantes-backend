b5cd29480da2fc0b2a74d97def75ee0135701c4a
JuanCalderon <Pest1lozzi>
2017-09-19 11:05:16 -0600

    Insertar solo empleados/proveedores nuevos

--- db/migrar_personas.py
+++ db/migrar_personas.py
@@ -15,6 +15,16 @@ try:
     result= curNetmultix.fetchone()
     existePersonas = result[0] == 1
 
+
+    sql_stmt =  """SELECT  DISTINCT 0 as id, pv01_clave as clave, 2 as tipo,  PV01_NOMBRE as nombre, CO05_UBICACION as sede FROM  co05,pv01
+                    WHERE CO05_CVE_PROV =PV01_CLAVE
+                    AND CO05_PEDIDO IN ( SELECT   CO06A_PEDIDO FROM co06a WHERE ( CO06A_CANT_PED - CO06A_CANT_REC) >= 0 and CO06A_PARTIDA = '33901' and CO06A_STATUS < 7)
+                    GROUP BY clave
+                  UNION
+                    select 0 as id, no01_cve_emp as clave, 1 as tipo, NO01_NOM_EMP as nombre, NO01_UBICACION as sede from no01 where NO01_STATUS = 'A'
+                  ORDER BY tipo, sede, nombre;"""
+
+
     if not existePersonas:
         curNetmultix.execute("""CREATE TABLE personas (
                 id INT NOT NULL,
@@ -44,16 +54,26 @@ try:
 
         # curNetmultix.execute(""" delimiter ;""")
 
-        curNetmultix.execute("""
-            insert into personas
-                SELECT  DISTINCT 0 as id, pv01_clave as clave, 2 as tipo,  PV01_NOMBRE as nombre, CO05_UBICACION as sede FROM  co05,pv01
-                WHERE CO05_CVE_PROV =PV01_CLAVE
-                AND CO05_PEDIDO IN ( SELECT   CO06A_PEDIDO FROM co06a WHERE ( CO06A_CANT_PED - CO06A_CANT_REC) >= 0 and CO06A_PARTIDA = '33901' and CO06A_STATUS < 7)
-                GROUP BY clave
-            union
-                select 0 as id, no01_cve_emp as clave, 1 as tipo, NO01_NOM_EMP as nombre, NO01_UBICACION as sede from no01 where NO01_STATUS = 'A'
-            ORDER BY tipo, sede, nombre;
-        """)
+        curNetmultix.execute("""insert into personas """ + sql_stmt)
+
+    else:
+
+        curNetmultixPersonas = cnnNetmultix.cursor()
+
+        curNetmultix.execute(sql_stmt)
+        for empleado_proveedor in curNetmultix.fetchall():
+            clave = empleado_proveedor[1].strip()
+            curNetmultixPersonas.execute("""SELECT * FROM personas WHERE clave = '%s';""" % (clave))
+            existePersona = curNetmultixPersonas.fetchone()
+            if existePersona is None:
+                tipo = empleado_proveedor[2]
+                nombre = empleado_proveedor[3].strip()
+                sede = empleado_proveedor[4]
+                sql_insert = """INSERT INTO personas VALUES (0,'%s',%s,'%s',%s);""" % (clave,tipo,nombre,sede)
+                curNetmultixPersonas.execute(sql_insert)
+
+        curNetmultixPersonas.close()
+
 
     curNetmultix.close()
     cnnNetmultix.commit()
