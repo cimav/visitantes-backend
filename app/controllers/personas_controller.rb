4691962381f757580b5029d74d0c4b0408bcdd1f
JuanCalderon <Pest1lozzi>
2018-07-06 10:51:03 -0600

    Personas incluye subcontratados (meses que faltan para terminar contrato)

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -9,14 +9,14 @@ class PersonasController < ApplicationController
 
   def proveedores
 
-    personas = Persona.where("tipo = 2 AND sede = :sede", {:sede=>params[:sede]}).order(:nombre)
+    personas = Persona.where("tipo = 2 AND meses_subcontrato > 0 AND sede = :sede", {:sede=>params[:sede]}).order(:meses_subcontrato, :nombre)
 
     render json: personas
   end
 
   private
   def person_params
-    params.require(:persona).permit(:id, :clave, :nombre, :tipo, :sede)
+    params.require(:persona).permit(:id, :clave, :nombre, :tipo, :sede, :meses_subcontrato)
   end
 end
 
