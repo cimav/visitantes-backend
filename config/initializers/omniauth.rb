76158f01beda553b9b994fb4fe7877ed87af2c1f
JuanCalderon <juan.calderon@gmail.com>
2019-11-14 16:09:19 -0700

    Evitar csfr

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {access_type: 'online', approval_prompt: '', :skip_jwt => true}
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {access_type: 'online', approval_prompt: '', :provider_ignores_state => true, :skip_jwt => true}
 end
